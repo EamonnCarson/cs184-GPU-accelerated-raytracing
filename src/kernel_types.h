e0e274ebb940b8d6c3fa70402f995f87160f1c2a
William Sheu <sheu.william@gmail.com>
2019-04-29 17:59:59 -0700

    Switch to Windows-compatible struct pack

--- src/kernel_types.h
+++ src/kernel_types.h
@@ -8,18 +8,20 @@
 
 /* Structs that are passed from host -> device */
 
-typedef struct __attribute__ ((packed)) kernel_mat3 {
+#pragma pack(push, 1)
+
+typedef struct kernel_mat3 {
   cl_float3 r0, r1, r2;
 } kernel_mat3_t;
 
-typedef struct __attribute__ ((packed)) kernel_camera {
+typedef struct kernel_camera {
   cl_float h_fov, v_fov, n_clip, f_clip;
   cl_float3 pos;
   kernel_mat3_t c2w;
 } kernel_camera_t;
 
 #define KERNEL_PRIMITIVE_TYPE_SPHERE 0
-typedef struct __attribute__ ((packed)) kernel_sphere {
+typedef struct kernel_sphere {
   cl_uchar type;
   cl_float3 origin;
   cl_float radius;
@@ -27,17 +29,19 @@ typedef struct __attribute__ ((packed)) kernel_sphere {
 } kernel_sphere_t;
 
 #define KERNEL_PRIMITIVE_TYPE_TRIANGLE 1
-typedef struct __attribute__ ((packed)) kernel_triangle {
+typedef struct kernel_triangle {
   cl_uchar type;
   cl_float3 vertices[3];
   cl_float3 normals[3];
 } kernel_triangle_t;
 
-typedef union __attribute__ ((packed)) kernel_primitive {
+typedef union kernel_primitive {
   cl_uchar type;
   kernel_sphere_t sphere;
   kernel_triangle_t triangle;
 } kernel_primitive_t;
+
+#pragma pack(pop)
 
 cl_float3 cglVectorToKernel(CGL::Vector3D vector);
 
